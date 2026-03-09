27a2bc2a3982020d5fe4f93f8be3797e0bf0e7ef
Sushma Palle <sushibytesstudios@gmail.com>
2026-03-09 14:10:35 -0400

    fix api route build error

--- app/api/pools/route.ts
+++ app/api/pools/route.ts
@@ -1,12 +1,18 @@
 export async function GET() {
-const baseId = process.env.AIRTABLE_BASE_ID
-const tableName = process.env.AIRTABLE_TABLE_NAME
-const apiKey = process.env.AIRTABLE_API_KEY
+const baseId = process.env.AIRTABLE_BASE_ID;
+const tableName = process.env.AIRTABLE_TABLE_NAME;
+const apiKey = process.env.AIRTABLE_API_KEY;
 
-const url = `https://api.airtable.com/v0/${baseId}/${tableName}`
+const url = `https://api.airtable.com/v0/${baseId}/${tableName}`;
 
 const response = await fetch(url, {
 headers: {
 Authorization: `Bearer ${apiKey}`,
 },
-})
+});
+const data = await response.json();
+
+return new Response(JSON.stringify(DataTransfer.records),{
+    headers: { "Content-Type": "application/json"};
+});
+}
