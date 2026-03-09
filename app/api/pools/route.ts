91aa5140b9cb1d14616c7d57d8cf5015ff5d6ab2
Sushma Palle <sushibytesstudios@gmail.com>
2026-03-09 13:13:58 -0400

    fix airtable api

--- app/api/pools/route.ts
+++ app/api/pools/route.ts
@@ -1,18 +1,12 @@
 export async function GET() {
-    const baseID = process.env.AIRTABLE_BASE_ID
-    const tableName = process.env.AIRTABLE_TABLE_NAME
-    const apiKey = process.env.AIRTABLE_API_KEY
+const baseId = process.env.AIRTABLE_BASE_ID
+const tableName = process.env.AIRTABLE_TABLE_NAME
+const apiKey = process.env.AIRTABLE_API_KEY
 
-    const url = 'https://api.airtable.com/v0/${baseID}/${tableName}'
+const url = `https://api.airtable.com/v0/${baseId}/${tableName}`
 
-    const response = await fetch(url, {
-        headers: {
-            Authorization: 'Bearer ${apiKey}' ,
-        },
-        })
-
-        const data = await response.json()
-
-        return Response.json(data.records)
-    }
-}
+const response = await fetch(url, {
+headers: {
+Authorization: `Bearer ${apiKey}`,
+},
+})
